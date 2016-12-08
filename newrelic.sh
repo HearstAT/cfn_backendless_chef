b6e22f2f4adca218d6ad9400a49ba283a08d121c
Levi Smith <levi@fynx.me>
2016-12-08 12:24:25 -0600

    Adding fixes to Template & Userdata found during testing and fixing issues with newrelic script found during testing

--- newrelic.sh
+++ newrelic.sh
@@ -7,17 +7,29 @@
 #
 ################################
 
+##################################################
+# Script Argument Set
+##################################################
+
 LICENSE_KEY=${1}
 APP_NAME=${2}
 DBURL=${3}
 DBUSER=${4}
 DBNAME=${5}
 
+#######################################################
+# Setup Newrelic Source List & Install System Monitor
+#######################################################
+
 echo 'deb http://apt.newrelic.com/debian/ newrelic non-free' >> /etc/apt/sources.list.d/newrelic.list || error_exit "Failed to add New Relic to sources"
 wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add - || error_exit "Failes to add New Relic GPG Source Key"
-apt-get update && apt-get install newrelic-sysmond || error_exit "Failed to install new relic system monitor"
+apt-get update && apt-get install newrelic-sysmond libpq-dev || error_exit "Failed to install new relic system monitor"
 
 nrsysmond-config --set license_key=${LICENSE_KEY} || error_exit "Failed to set New Relic License on System Monitor"
+
+##################################################
+# Install and Setup New Relic Plugin Agent
+##################################################
 
 pip install newrelic-plugin-agent || error_exit "Failed to install newrelic plugin agent"
 
@@ -87,13 +99,17 @@ PIDFile=/var/run/newrelic/newrelic-plugin-agent.pid
 ExecStartPre=/bin/mkdir -p /var/run/newrelic /var/log/newrelic
 ExecStartPre=/bin/rm -f /var/run/newrelic/newrelic-plugin-agent.pid
 ExecStartPre=/bin/chown -R newrelic:newrelic /var/run/newrelic
-ExecStart=/usr/bin/newrelic-plugin-agent -c /etc/newrelic/newrelic-plugin-agent.cfg
+ExecStart=/usr/local/bin/newrelic-plugin-agent -c /etc/newrelic/newrelic-plugin-agent.cfg
 ExecStop=/bin/kill -INT $MAINPID
 Restart=on-abort
 
 [Install]
 WantedBy=multi-user.target
 EOF
+
+##################################################
+# Configure New Relic APM Options
+##################################################
 
 cat > '/root/newrelic.yml' << EOF
 production:
@@ -105,6 +121,10 @@ EOF
 
 cp -f /root/newrelic.yml /var/opt/chef-manage/etc/
 cp -f /root/newrelic.yml /opt/opscode/embedded/service/oc_id/config/
+
+##################################################
+# Enable and Start all New Relic Agents
+##################################################
 
 systemctl enable newrelic-sysmond || error_exit "Failed to enable New Relic System Monitor"
 systemctl enable newrelic-plugin-agent || error_exit "Failed to Enable New Relic Plugin Agent"
